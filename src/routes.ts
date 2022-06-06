48d058e5480fc3b0195d16241e5b28a653993126
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-06 20:39:20 -0300

    refatoração adicionando eslint

--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import createCourseService from "./CreateCourseService";
-
-export function createCourse(req: Request, res: Response){
- const course =  createCourseService.execute('Node', 46, 'Leo');
- return res.json(course);
-}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import express from 'express';
-import { createCourse } from './routes';
 
 const app = express();
-
-app.get('/', createCourse)
 
 app.listen(3333);
