8ff67bc0acb7b880ee6f1fff14041b9844eda521
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-15 08:35:19 -0300

    refatoração adicionando o conceito de usecases

---
+++ src/modules/cars/categories/usecases/ListCategory/ListCategoriesUseCase.ts
@@ -0,0 +1,17 @@
+import { Category } from '../../model/Category';
+import { ICategoryRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoryRepository) {}
+  public execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    if (!categories.length) {
+      throw new Error('Not categories found!');
+    }
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
