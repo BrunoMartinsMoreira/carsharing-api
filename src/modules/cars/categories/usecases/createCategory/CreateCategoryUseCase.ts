f20836eb6454f6fac14fff0f130565c06bc0b076
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-15 08:35:02 -0300

    refatoração adicionando o conceito de usecases

---
+++ src/modules/cars/categories/usecases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,28 @@
+import { ICategoryRepository } from '../../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryUseCase {
+  constructor(private categoriesRepository: ICategoryRepository) {}
+
+  public async execute({
+    name,
+    description,
+  }: IRequest): Promise<Promise<void>> {
+    const categoryExists = await this.categoriesRepository.findByName(name);
+
+    if (categoryExists) {
+      throw new Error('Category already exists');
+    }
+
+    this.categoriesRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateCategoryUseCase };
