6eeedd2e01dbb41e564979fcfc274fb321d0a733
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-25 22:46:28 -0300

    add & config typeorm

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 app.use(express.json());
