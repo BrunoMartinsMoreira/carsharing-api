dd3e83f7c5cadba882b424da5fc6a15433989e40
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-04 20:18:09 -0300

    cria container Specification

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import { CategoriesRepository } from '../../modules/cars/categories/repositories/CategoriesRepository';
 import { ICategoryRepository } from '../../modules/cars/categories/repositories/ICategoriesRepository';
+import { ISpecificationRepository } from '../../modules/cars/specifications/repositories/ISpecificationsRepository';
+import { SpecificationRepository } from '../../modules/cars/specifications/repositories/SpecificationRepository';
 
 container.registerSingleton<ICategoryRepository>(
   'CategoriesRepository',
   CategoriesRepository,
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  'SpecificationRepository',
+  SpecificationRepository,
 );
