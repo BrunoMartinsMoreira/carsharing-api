b0fdca9d7125c238e2cd2038eb36f7589bab6631
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-06 20:31:54 -0300

    add user repository

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUsersDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUserRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    name,
+    username,
+    password,
+    email,
+    driver_licence,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      password,
+      email,
+      driver_licence,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
