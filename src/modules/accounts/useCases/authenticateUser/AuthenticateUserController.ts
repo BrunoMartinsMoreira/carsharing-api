e0c341c8c30a4f2f78a6c44b77c20348cff3fbed
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-07 22:22:09 -0300

    criado controller de autenticação

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+class AuthenticateUserController {
+  public async handle(req: Request, res: Response): Promise<Response> {
+    try {
+      const { email, password } = req.body;
+      const authenticateUserUseCase = container.resolve(
+        AuthenticateUserUseCase,
+      );
+
+      const auth = await authenticateUserUseCase.execute({ email, password });
+
+      return res.status(200).json(auth);
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+  }
+}
+
+export { AuthenticateUserController };
