72a864f1f8724f43093a637e8926e800ebdc82c1
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-07 22:23:51 -0300

    add rota de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { authenticateRoutes } from './modules/accounts/authenticate.routes';
 import { usersRoutes } from './modules/accounts/users.routes';
 import { categoriesRoutes } from './modules/cars/categories/categories.routes';
 import { specificationsRoutes } from './modules/cars/specifications/specs.routes';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', usersRoutes);
+routes.use('/sessions', authenticateRoutes);
 
 export { routes };
