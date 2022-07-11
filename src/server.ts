5a9dc79f59ad0ff4f6940f69c5ac08983d34de36
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-11 19:08:07 -0300

    add custum error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
+import { AppError } from './errors/AppError';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 import 'express-async-errors';
@@ -14,5 +16,19 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/', routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server is running'));
