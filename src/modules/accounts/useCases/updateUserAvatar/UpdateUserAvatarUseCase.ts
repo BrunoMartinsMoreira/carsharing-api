32e7eff446a97ed86afe19636c72e172b91e99ec
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-13 19:33:39 -0300

    feat: create feature to upload avatar file

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../errors/AppError';
+import { IUsersRepository } from '../../repositories/IUserRepository';
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!avatar_file) {
+      throw new AppError('File is required');
+    }
+
+    if (user) {
+      user.avatar_url = avatar_file;
+      await this.usersRepository.create(user);
+    }
+  }
+}
+
+export { UpdateUserAvatarUseCase };
