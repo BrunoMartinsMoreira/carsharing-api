c524101fce8d44c3b612ec004a529f531f894b82
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-13 19:36:02 -0300

    feat: create route to upload avatar and add directory tmp in gitignore

--- src/modules/accounts/users.routes.ts
+++ src/modules/accounts/users.routes.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../../config/upload';
+
+import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated';
 import { CreateUserController } from './useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from './useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const createUserController = new CreateUserController();
+const upadateAvatarController = new UpdateUserAvatarController();
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  upadateAvatarController.handle,
+);
 
 export { usersRoutes };
