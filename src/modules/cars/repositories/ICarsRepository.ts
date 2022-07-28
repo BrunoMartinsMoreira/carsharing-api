eafc1e39236fbf2a181f5ed15035b7b1715a6fbc
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-07-28 12:52:17 -0300

    refactor: modifies the create method of the car repository so that it is possible to capture the information registered in the methods return

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,4 +1,5 @@
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
+import { Car } from '../infra/typeorm/entities/Car';
 
 interface ICarsRepository {
   create({
@@ -9,7 +10,9 @@ interface ICarsRepository {
     fine_amount,
     brand,
     category_id,
-  }: ICreateCarDTO): Promise<void>;
+  }: ICreateCarDTO): Promise<Car>;
+
+  findByLicencePlate(id: string): Promise<Car | undefined>;
 }
 
 export { ICarsRepository };
