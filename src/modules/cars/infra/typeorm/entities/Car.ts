70d0ec847d42a5b5534ca9ebf5d5fe6e156e5b74
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-08-01 07:22:35 -0300

    feat: Create entity Car

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { Category } from './Category';
+
+@Entity('cars')
+class Car {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column()
+  available = true;
+
+  @Column()
+  licence_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'categody_id' })
+  category: Category;
+
+  @Column()
+  category_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Car };
