859cd62dc857d6d872d001b58842687918f50a5d
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-09-04 18:51:27 -0300

    refactor: refactoring implementing refresh token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,7 +2,9 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 import { AppError } from '../../../../shared/errors/AppError';
+import { IDateProvider } from '../../../../shared/providers/datePovider/IDateProvider';
 import { IUsersRepository } from '../../repositories/IUserRepository';
+import { IUsersTokensRepository } from '../../repositories/IUsersTokensRepository';
 
 interface IRequest {
   email: string;
@@ -17,6 +19,7 @@ interface IResponse {
     isAdmin: boolean;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
@@ -24,9 +27,22 @@ class AuthenticateUserUseCase {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject('DayjsProvider')
+    private dateProvider: IDateProvider,
   ) {}
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
+
+    const {
+      JWT_SECRET_KEY,
+      TOKEN_EXPIRATION,
+      REFRESH_TOKEN_SECRET_KEY,
+      REFRESH_TOKEN_EXPIRATION,
+    } = process.env;
 
     if (!user) {
       throw new AppError('Incorret email or password', 401);
@@ -38,9 +54,22 @@ class AuthenticateUserUseCase {
       throw new AppError('Incorret email or password', 401);
     }
 
-    const token = sign({}, String(process.env.JWT_SECRET_KEY), {
+    const token = sign({}, String(JWT_SECRET_KEY), {
       subject: user.id,
-      expiresIn: '7d',
+      expiresIn: String(TOKEN_EXPIRATION),
+    });
+
+    const refresh_token = sign({ email }, String(REFRESH_TOKEN_SECRET_KEY), {
+      subject: user.id,
+      expiresIn: String(REFRESH_TOKEN_EXPIRATION),
+    });
+
+    const expires_date = this.dateProvider.addDays(30);
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      expires_date,
+      refresh_token,
     });
 
     const tokenResponse: IResponse = {
@@ -51,6 +80,7 @@ class AuthenticateUserUseCase {
         isAdmin: user.isAdmin,
       },
       token,
+      refresh_token,
     };
     return tokenResponse;
   }
