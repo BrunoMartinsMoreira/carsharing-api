f7543764cfb9c406b7f823387027f7250c7ad5d7
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-11-18 22:59:00 -0300

    feat: user profile route

--- src/modules/accounts/infra/http/routes/users.routes.ts
+++ src/modules/accounts/infra/http/routes/users.routes.ts
@@ -4,16 +4,19 @@ import uploadConfig from '../../../../../config/upload';
 
 import { ensureAuthenticated } from '../../../../../shared/infra/http/middlewares/ensureAuthenticated';
 import { CreateUserController } from '../../../useCases/createUser/CreateUserController';
+import { ProfileUserController } from '../../../useCases/profileUser/ProfileUserController';
 import { UpdateUserAvatarController } from '../../../useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const createUserController = new CreateUserController();
 const upadateAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 const usersRoutes = Router();
 
 const uploadAvatar = multer(uploadConfig);
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.get('/profile', profileUserController.handle);
 
 usersRoutes.patch(
   '/avatar',
